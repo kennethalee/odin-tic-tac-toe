ec60ecae62fb44c48bcd84dcae08bd01ee03b4ce
Kenneth Aidan Lee <158255466+kennethalee@users.noreply.github.com>
2024-12-16 12:23:02 +0900

    simplify to reduce metric complexity

--- lib/board.rb
+++ lib/board.rb
@@ -6,62 +6,62 @@ class Board
       [7, 8, 9]
     ]
   end
-  
+
   def display
     @board.each_with_index do |row, index|
       puts row.join(' | ')
-      puts "--+---+--" unless index == @board.size - 1
+      puts '--+---+--' unless index == @board.size - 1
     end
   end
 
   def valid_move?(position)
-    if position > 9
-      return false
-    else
-      true
-    end
+    return false if position > 9
+
+    true
   end
 
   def update(position, symbol)
-    if valid_move?(position)
-      row = (position - 1) / 3
-      col = (position - 1) % 3
-      @board[row][col] = symbol
-    end
+    return unless valid_move?(position)
+
+    row = (position - 1) / 3
+    col = (position - 1) % 3
+    @board[row][col] = symbol
   end
 
   def check_winner
-    # check row 
-    return true if @board.any? {
-      |row| row.uniq.size == 1 && ["X", "O"].include?(row.first) 
-    }
+    if row_winner || column_winner || diagonal_winner
+      true
+    else
+      false
+    end
+  end
 
-    # check column
+  def row_winner
+    return true if @board.each do |row|
+      row.uniq.size == 1 && %w[X O].include?(row.first)
+    end
+
+    false
+  end
+
+  def column_winner
     transposed_board = @board.transpose
-    return true if transposed_board.any? {
-      |col| col.uniq.size == 1 && ["X", "O"].include?(col.first)
-    }
-    
-    # check diagonal
+    return true if transposed_board.any? do |col|
+      col.uniq.size == 1 && %w[X O].include?(col.first)
+    end
+
+    false
+  end
+
+  def diagonal_winner
     n = @board.size
     primary_diagonal = (0...n).map { |i| @board[i][i] }
     secondary_diagonal = (0...n).map { |i| @board[i][n - 1 - i] }
 
-    return true if [primary_diagonal, secondary_diagonal].any? {
-      |diagonal| diagonal.uniq.size == 1 && ["X", "O"].include?(diagonal.first)
-    }
+    return true if [primary_diagonal, secondary_diagonal].any? do |diagonal|
+      diagonal.uniq.size == 1 && %w[X O].include?(diagonal.first)
+    end
 
     false
   end
 end
-
-
-# Tests
-test = Board.new()
-test.update(3, "X")
-test.update(6, "X")
-test.update(0, "X")
-test.update(1, "X")
-test.display
-p test.check_winner
-
